daf7e5e69a6933eb0030f32b9e9ac1b918c724e8
michal <michwey@gmail.com>
2025-02-05 21:11:21 +0100

    send/receive

--- src/discover.exercise.js
+++ src/discover.exercise.js
@@ -1,16 +1,23 @@
 /** @jsx jsx */
 import {jsx} from '@emotion/core'
 
+import { useState, useEffect } from 'react'
 import './bootstrap'
 import Tooltip from '@reach/tooltip'
 import {FaSearch} from 'react-icons/fa'
 import {Input, BookListUL, Spinner} from './components/lib'
 import {BookRow} from './components/book-row'
+import {client} from './utils/api-client'
 // 🐨 import the client from './utils/api-client'
+
+let queriedFor = '';
 
 function DiscoverBooksScreen() {
   // 🐨 add state for status ('idle', 'loading', or 'success'), data, and query
-  const data = null // 💣 remove this, it's just here so the example doesn't explode
+  const [status, setStatus] = useState('idle');
+  const [data, setData] = useState();
+  const [queried, setQueried] = useState(false);
+  const [query, setQuery] = useState();
   // 🐨 you'll also notice that we don't want to run the search until the
   // user has submitted the form, so you'll need a boolean for that as well
   // 💰 I called it "queried"
@@ -22,11 +29,55 @@ function DiscoverBooksScreen() {
   // so you'll want to check if the user has submitted the form yet and if
   // they haven't then return early (💰 this is what the queried state is for).
 
+  useEffect(() => {
+
+    if (!queried) {
+      setStatus('idle');
+      return;
+    }
+
+    if (queried && queriedFor === query) {
+      console.log('Duplicate query, ignoring: ', query)
+      return;
+    } else {
+      console.log(`rerunning queried:${queried}, queriedFor:${queriedFor}, query:${query}`);
+    }
+
+    queriedFor = query;
+    setStatus('loading');
+    client(`books?query=${encodeURIComponent(query)}`).then((json) => {
+      if (queriedFor !== query) {
+        return;
+      }
+      setData(json);
+      setStatus('success')
+    }).catch((e) => {
+      setQueried(false);
+      setStatus('idle');
+      setData({})
+    })
+
+    return () => {
+      // does nothing
+    }
+
+  }, [queried, query]);
+
+
   // 🐨 replace these with derived state values based on the status.
-  const isLoading = false
-  const isSuccess = false
+  const isLoading = (status === 'loading');
+  const isSuccess = (status === 'success' );
 
   function handleSearchSubmit(event) {
+    event.preventDefault()
+    setQueried(true);
+
+    console.log("Form elements: ", event.target.elements);
+    const field = event.target.elements['search'];
+    const text = field.value;
+
+    setQuery(text);
+
     // 🐨 call preventDefault on the event so you don't get a full page reload
     // 🐨 set the queried state to true
     // 🐨 set the query value which you can get from event.target.elements
--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,4 +1,4 @@
-function client(endpoint, customConfig = {}) {
+async function client(endpoint, customConfig = {}) {
   // 🐨 create the config you'll pass to window.fetch
   //    make the method default to "GET"
   // 💰 if you're confused by this, that's fine. Scroll down to the bottom
@@ -6,9 +6,24 @@ function client(endpoint, customConfig = {}) {
   // 🐨 call window.fetch(fullURL, config) then handle the json response
   // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
   // 💰 here's how to get the full URL: `${process.env.REACT_APP_API_URL}/${endpoint}`
+
+    let config = {
+        method: "GET",
+        ...customConfig
+    }
+
+    console.log("Requesting - ", endpoint);
+    const resp = await fetch(`${process.env.REACT_APP_API_URL}/${endpoint}`, config);
+    if (!resp.ok) {
+        throw new Error(`Response status: ${resp.status}`)
+    }
+    const content = await resp.json();
+
+    return content;
 }
 
 export {client}
+
 
 /*
 
