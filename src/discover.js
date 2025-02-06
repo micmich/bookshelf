47def86b9b5756c04d5571e48640c1d82dfb8640
michal <michwey@gmail.com>
2025-02-06 22:38:09 +0100

    send/receive

--- src/discover.js
+++ src/discover.js
@@ -1,9 +1,9 @@
 // export * from './discover.final'
 
-export * from './discover.exercise'
+// export * from './discover.exercise'
 
 // 💯 handle failed requests
-// export * from './discover.extra-1'
+export * from './discover.extra-1'
 
 // 💯 use the useAsync hook
 // export * from './discover.extra-2'
