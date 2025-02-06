eb57caaf4205c2c96ccdc4a53dce85c2fbd36117
michal <michwey@gmail.com>
2025-02-06 23:22:04 +0100

    basic remote

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -2,35 +2,39 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
-// 🐨 you're going to need this:
 import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import {client} from './utils/api-client'
 
 function App() {
-  // 🐨 useState for the user
-  const [user, setUser] = React.useState();
+  const [user, setUser] = React.useState(null)
 
-  // 🐨 create a login function that calls auth.login then sets the user
   const login = form => auth.login(form).then(u => setUser(u))
-  // 🐨 create a registration function that does the same as login except for register
   const register = form => auth.register(form).then(u => setUser(u))
-  const logout = () => auth.logout().then(() => setUser(null))
-  // 🐨 create a logout function that calls auth.logout() and sets the user to null
-
-  // 🐨 if there's a user, then render the AuthenticatedApp with the user and logout
-  // 🐨 if there's not a user, then render the UnauthenticatedApp with login and register
-
-  if (user == undefined) {
-    return <UnauthenticatedApp login={login} register={register} />
-  } else {
-    return <AuthenticatedApp user={user} logout={logout} />
+  const logout = () => {
+    auth.logout()
+    setUser(null)
   }
+
+  React.useEffect(() => {
+    auth.getToken().then((token) => {
+      if (!token) {
+        setUser(null);
+        return;
+      }
+
+      client('me', {token}).then(data => {
+        setUser(data.user);
+      })
+    })
+  }, []);
+
+  return user ? (
+    <AuthenticatedApp user={user} logout={logout} />
+  ) : (
+    <UnauthenticatedApp login={login} register={register} />
+  )
 }
 
 export {App}
-
-/*
-eslint
-  no-unused-vars: "off",
-*/
