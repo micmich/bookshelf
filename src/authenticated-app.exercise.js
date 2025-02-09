6d3203a5848ba9ec2297d0d3c881b20f01439edb
michal <michwey@gmail.com>
2025-02-09 17:55:25 +0100

    match

--- src/authenticated-app.exercise.js
+++ src/authenticated-app.exercise.js
@@ -5,7 +5,7 @@ import * as React from 'react'
 // We'll be doing a lot of stuff with the router on this page.
 // 🐨 Here's what you'll need to import from react-router-dom
 // Routes, Route, Link
-import {Routes, Route, Link} from 'react-router-dom'
+import {Routes, Route, Link, useMatch } from 'react-router-dom'
 import {Button} from './components/lib'
 import * as mq from './styles/media-queries'
 import * as colors from './styles/colors'
@@ -61,23 +61,38 @@ function AuthenticatedApp({user, logout}) {
 
 function NavLink(props) {
   // 🐨 change this from an <a /> to a <Link />
+
+  const { to } = props;
+  const matches = useMatch(to)
+
   return (
     <Link
-      css={{
-        display: 'block',
-        padding: '8px 15px 8px 10px',
-        margin: '5px 0',
-        width: '100%',
-        height: '100%',
-        color: colors.text,
-        borderRadius: '2px',
-        borderLeft: '5px solid transparent',
-        ':hover': {
-          color: colors.indigo,
-          textDecoration: 'none',
-          background: colors.gray10,
-        },
-      }}
+      css={[
+        {
+          display: 'block',
+          padding: '8px 15px 8px 10px',
+          margin: '5px 0',
+          width: '100%',
+          height: '100%',
+          color: colors.text,
+          borderRadius: '2px',
+          borderLeft: '5px solid transparent',
+          ':hover': {
+            color: colors.indigo,
+            textDecoration: 'none',
+            background: colors.gray10,
+          },
+        }
+        ,{
+          ...(!matches ? {} : {
+            borderLeft: `5px solid ${colors.indigo}`,
+            background: colors.gray10,
+            ':hover': {
+              background: colors.gray20,
+            }
+          })
+        }
+      ]}
       {...props}
     />
   )
